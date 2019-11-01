aeac0c8fdc4cde2bcac6a1de29634acfe51d0dc5
Madhavi Tippani <madhavitippani28@gmail.com>
2019-11-01 16:18:37 -0400

    Update store_mat.m

--- Scripts/store_mat.m
+++ Scripts/store_mat.m
@@ -1,5 +1,6 @@
-function store_mat(filename)
+function store_mat(filename,bftools)
 
+addpath(genpath(bftools))
 out1 = ReadImage6D(filename);
 image6d1 = out1{1}; %dims = series,time, z, c, x, y
 rawImg = permute(squeeze(image6d1),[2,3,1]);
