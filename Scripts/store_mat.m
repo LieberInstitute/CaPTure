31f61cd34f92a16d7e9c12a36dbe1c05a6972556
Madhavi Tippani <madhavitippani28@gmail.com>
2019-11-01 16:14:14 -0400

    Create store_mat.m

---
+++ Scripts/store_mat.m
@@ -0,0 +1,9 @@
+function store_mat(filename)
+
+out1 = ReadImage6D(filename);
+image6d1 = out1{1}; %dims = series,time, z, c, x, y
+rawImg = permute(squeeze(image6d1),[2,3,1]);
+name4 =fullfile(greenfiles, [name,'.mat']);
+save(filename(1:end-4),'rawImg','-v7.3');
+
+end
