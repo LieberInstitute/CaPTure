86ae49ff55c2fd0b7099073439181d3938a12041
Madhavi Tippani <madhavitippani28@gmail.com>
2020-03-23 10:58:29 -0400

    Update store_mat.m

--- toolbox/store_mat.m
+++ toolbox/store_mat.m
@@ -6,7 +6,7 @@ addpath(genpath(bftools))
 out1 = ReadImage6D(filename);
 image6d1 = out1{1}; %dims = series,time, z, c, x, y
 rawImg = permute(squeeze(image6d1),[2,3,1]);
-save(filename(1:end-4),'rawImg','-v7.3');
+save([filename(1:end-4),'.mat'],'rawImg','-v7.3');
 warning('on','all');
 toc
 
